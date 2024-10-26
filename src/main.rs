2b214050ae02460e5f523253075aeb7e733c431e
regularpoe <vedranvinko@protonmail.com>
2024-10-26 19:17:12 +0200

    Setup CLI and basic fn stubs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use chrono::NaiveDate;
+use clap::{Parser, Subcommand};
 use rusqlite::{Connection, Result};
+
+#[derive(Parser)]
+#[command(name = "y")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { date: String, description: String },
+    ViewAll,
+    View { date: String },
+    Edit { id: i32, description: String },
+    Delete { id: i32 },
+}
 
 #[derive(Debug)]
 struct Log {
@@ -22,8 +39,50 @@ fn init_db() -> Result<Connection> {
     Ok(conn)
 }
 
+// CRUD
+
+fn add() {
+    println!("Add called");
+}
+
+fn view_all() {
+    println!("View all called");
+}
+
+fn view_by_date() {
+    println!("View by date called");
+}
+
+fn edit() {
+    println!("Edit called");
+}
+
+fn delete() {
+    println!("Delete called");
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
     let conn = init_db()?;
+
+    match cli.command {
+        Commands::Add { date, description } => {
+            add();
+        }
+        Commands::ViewAll => {
+            view_all();
+        }
+        Commands::View { date } => {
+            view_by_date();
+        }
+        Commands::Edit { id, description } => {
+            edit();
+        }
+        Commands::Delete { id } => {
+            delete();
+        }
+    }
 
     Ok(())
 }
