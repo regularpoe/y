23f169ca868bf58add7cf5ae7519e48fc68e6513
regularpoe <vedranvinko@protonmail.com>
2024-10-26 18:58:48 +0200

    init_db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::NaiveDate;
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
+struct Log {
+    id: i32,
+    date: NaiveDate,
+    description: String,
+}
+
+fn init_db() -> Result<Connection> {
+    let conn = Connection::open("y.db")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS logs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            description TEXT NOT NULL
+        )",
+        [],
+    )?;
+
+    Ok(conn)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let conn = init_db()?;
+
+    Ok(())
 }
